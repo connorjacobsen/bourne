06a4b8e8c2872448d32aff528d8b5d332ebca32b
Connor Jacobsen <jacobsen.connor@gmail.com>
2017-09-30 11:56:36 -0700

    add julia alias

--- aliases/osx.aliases.sh
+++ aliases/osx.aliases.sh
@@ -5,3 +5,6 @@ alias f='open -a Finder '
 # Mute/Unmute the system volume. Plays nice with all other volume settings.
 alias mute="osascript -e 'set volume output muted true'"
 alias unmute="osascript -e 'set volume output muted false'"
+
+# Open julia
+alias julia="exec '/Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia'"
